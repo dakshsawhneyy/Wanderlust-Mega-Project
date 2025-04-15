2ab3f98e9df37e44095163246375a7efca0bd839
Daksh Sawhney <darkstardaksh.com>
2025-04-15 09:42:54 +0000

    Terraform Changed

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_key_pair" "deployer" {
   key_name   = "terra-automate-key"
-  public_key = file("/Users/shubham/Documents/work/TrainWithShubham/terra-practice/terra-key.pub")
+  public_key = file("/mnt/c/Studies/DevOps/terraform_for_devops/tf-key.pub")
 }
 
 resource "aws_default_vpc" "default" {
@@ -11,6 +11,7 @@ resource "aws_security_group" "allow_user_to_connect" {
   name        = "allow TLS"
   description = "Allow user to connect"
   vpc_id      = aws_default_vpc.default.id
+
   ingress {
     description = "port 22 allow"
     from_port   = 22
